f137ef7f61443958c7247bb4032001ed5e851903
Mundru Pragna <pragnamundru@gmail.com>
2024-07-30 14:59:26 -0500

    commit filee

--- src/main/java/com/mgcloudtech/controller/EmployeesController.java
+++ src/main/java/com/mgcloudtech/controller/EmployeesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employ")
 public class EmployeesController {
 
     @Autowired
